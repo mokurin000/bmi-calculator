f7911393269893cbd93b53788aae599a56be5493
poly000 <1348292515@qq.com>
2021-08-29 21:54:26 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,24 @@ fn main() {
         "身高（厘米）",
         (0, i32::MAX),
     ) {
-        let height: f64 = height as f64 / 100.0;
-        let bmi = weight as f64 / height.powi(2);
-
-        let status = match bmi {
-            _ if bmi < 18.5 => "过轻",
-            _ if bmi < 24.0 => "正常",
-            _ if bmi < 28.0 => "超重（亚肥胖）",
-            _ if bmi < 30.0 => "肥胖一级",
-            _ if bmi < 40.0 => "肥胖二级",
-            _ => "肥胖三级",
-        };
+        let (bmi, status) = bmi_calculate(weight, height);
         msg("BMI", bmi.to_string().as_str());
         msg("身体状况", status);
     }
+}
+
+fn bmi_calculate(weight: i32, height: i32) -> (f64, &'static str) {
+    let height: f64 = height as f64 / 100.0;
+    let bmi = weight as f64 / height.powi(2);
+
+    let status = match bmi {
+        _ if bmi < 18.5 => "过轻",
+        _ if bmi < 24.0 => "正常",
+        _ if bmi < 28.0 => "超重（亚肥胖）",
+        _ if bmi < 30.0 => "肥胖一级",
+        _ if bmi < 40.0 => "肥胖二级",
+        _ => "肥胖三级",
+    };
+
+    (bmi, status)
 }
