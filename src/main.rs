c9193035f3be65fb067633630fb70659f627ce02
poly000 <1348292515@qq.com>
2021-08-31 01:32:47 +0800

    Fix UI

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ use ndless::prelude::*;
 fn main() {
     while let Some((weight, height)) = msg_2numeric(
         "身体数据",
-        "",
+        "请输入您的身体数据",
         "体重 (公斤)",
         (0, i32::MAX),
         "身高 (厘米)",
         (0, i32::MAX),
     ) {
         let (bmi, status) = bmi_calculate(weight, height);
-        msg("BMI", bmi.to_string().as_str());
+        msg("身体质量指数", bmi.to_string().as_str());
         msg("身体状况", status);
     }
 }
@@ -25,9 +25,9 @@ fn bmi_calculate(weight: i32, height: i32) -> (f64, &'static str) {
     let bmi = weight as f64 / height.powi(2);
 
     let status = match bmi {
-        _ if bmi < 18.5 => "过轻",
-        _ if bmi < 24.0 => "正常",
-        _ if bmi < 28.0 => "超重 (亚肥胖)",
+        _ if bmi < 18.5 => "体重过轻",
+        _ if bmi < 24.0 => "体重正常",
+        _ if bmi < 28.0 => " 亚肥胖 ",
         _ if bmi < 30.0 => "肥胖一级",
         _ if bmi < 40.0 => "肥胖二级",
         _ if bmi.is_finite() => "肥胖三级",
